23e9ac234b7193aa15c6deaa1b40034269d12c3e
georgesroberto <georgesroberto21@gmail.com>
2024-09-09 12:30:09 +0300

    Prep for signals

--- tms/asgi.py
+++ tms/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tms.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tms.settings.development')
 
 application = get_asgi_application()
